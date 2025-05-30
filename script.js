bfdfccfad23b05ff5b742ab4f652557ba654b186
user.name like:"komal verma <user.emal "komalverma2212005@gmail.com>
2025-05-30 18:30:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuBar=document.getElementById("menu-icon");
+const shortCutE1=document.querySelector(".shortcut");
+console.log(shortCutE1);
+
+menuBar.addEventListener("click",()=>{
+    menuBar.classList.toggle("bx-x");
+    shortCutE1.classList.toggle('active')
+})
